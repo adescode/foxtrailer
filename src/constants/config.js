61920bc27ff43f0856c5c59eec3107485c0337cc
Adelaja Ibrahim Abiol(AiA) <adelaja444@gmail.com>
2019-10-07 12:18:56 +0100

    Api key changed

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,3 @@
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const API_KEY = '5c1403f25d3be3579b4e1f905470bcdf';
-
 import Config from 'react-native-config';
 
 export const TMDB_URL = Config.TMDB_URL;
